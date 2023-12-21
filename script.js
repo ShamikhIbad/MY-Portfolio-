c2e9a0ebc51a64a56f13742b2856d43992034677
Shamikh Ibad <shamikhibad3@gmail.com>
2023-12-22 02:56:33 +0500

    Media Queries 1200px ADDED

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-
-
-   
+function toggleMenu() {
+const menu = document.querySelector(".linesMenuLinks");   
+const icon = document.querySelector(".linesIcon");
+menu.classList.toggle("open");
+icon.classList.toggle("open");
+}
